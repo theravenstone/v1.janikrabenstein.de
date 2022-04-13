7d53aa61f55ad1af6674caca1097aa9978f659e4
theravenstone <theravenstonepsn@gmail.com>
2022-04-13 10:18:40 +0200

    aufräumen von styles

--- raven-frontend/src/components/Hero.vue
+++ raven-frontend/src/components/Hero.vue
@@ -9,17 +9,18 @@
           class="animate-wiggle mb-6 inline-block">👋</span></p>
     </div>
     <div class="hidden md:block">
-      <i id="triangle" class="fa-solid fa-play text-primary absolute"></i>
-      <i id="square" class="fa-solid fa-square text-primary absolute"></i>
-      <i id="circle" class="fa-solid fa-circle text-primary absolute"></i>
-      <a rel="noopener" target="_blank" href="https://github.com/theravenstone" title="Janik's GitHub Profil">
-        <i id="github" class="fa-brands fa-github text-primary hover:text-primary-hover transition duration-300 absolute"></i>
+      <i id="triangle" class="fa-solid fa-play text-primary absolute w-24 h-24"></i>
+      <i id="square" class="fa-solid fa-square text-primary absolute animate-rotate360 -right-24 w-64 h-64"></i>
+      <i id="circle" class="fa-solid fa-circle text-primary absolute w-24 h-24 bottom-3/4 left-1/3 animate-movecircle"></i>
+      <a rel="noopener" target="_blank" href="https://github.com/theravenstone" title="GitHub">
+        <i id="github" class="fa-brands fa-github text-primary hover:text-primary-hover transition duration-300 absolute w-16 h-16 top-1/3"></i>
       </a>
-      <i id="vuejs" class="fa-brands fa-vuejs text-primary absolute"></i>
-      <a rel="noopener noreferrer" target="_blank" href="https://discord.com/users/594922360553865216" title="Janik's Discord Account">
-        <i id="discord" class="fa-brands fa-discord text-primary hover:text-primary-hover transition duration-300 absolute"></i>
+      <i id="vuejs" class="fa-brands fa-vuejs text-primary absolute w-16 h-16 left-3/4 top-3/4"></i>
+      <a rel="noopener noreferrer" target="_blank" href="https://discord.com/users/594922360553865216" title="Discord">
+        <i id="discord" class="fa-brands fa-discord text-primary hover:text-primary-hover transition duration-300 absolute w-16 h-16 left-3/4 top-1/4"></i>
       </a>
     </div>
+    
     
     </section>
 </template>
@@ -39,24 +40,11 @@ export default {
 #triangle {
   animation: triangle-anim 10s cubic-bezier(0.4, 0, 0.2, 1) infinite;
   bottom: 15%;
-  height: 100px;
   left: 12%;
-  width: 100px;
-}
-
-@keyframes square-anim {
-  100% {
-    transform: rotate(360deg);
-  }
 }
 
 #square {
-  animation:square-anim 40s infinite linear;
-  bottom: 8%;
-  height: 250px;
-  width: 250px;
-  right: -100px;
-  
+  bottom: 8%; 
 }
 
 @keyframes circle-anim {
@@ -66,14 +54,7 @@ export default {
   }
 }
 
-#circle {
-  animation:circle-anim 10s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-  bottom: 70%;
-  width: 100px;
-  height: 100px;
-  left: 40%;
-  
-}
+
 @keyframes github-anim {
   50% {
     animation-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
@@ -82,11 +63,7 @@ export default {
 }
 #github {
   animation: github-anim 15s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-  height: 57px;
-  width: 76px;
   left: 13%;
-  top: 18%;
-  
 }
 @keyframes vuejs-anim {
   50% {
@@ -97,11 +74,6 @@ export default {
 
 #vuejs {
   animation: vuejs-anim 15s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-  height: 57px;
-  width: 76px;
-  left: 70%;
-  top: 65%;
-  
 }
 
 @keyframes discord-anim {
@@ -113,10 +85,5 @@ export default {
 
 #discord {
   animation: discord-anim 7s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-  height: 60px;
-  width: 60px;
-  left: 75%;
-  top: 25%;
-
 }
 </style>
--- raven-frontend/tailwind.config.js
+++ raven-frontend/tailwind.config.js
@@ -15,10 +15,18 @@ module.exports = {
                 wiggle: {
                     '0%, 100%': { transform: 'rotate(-3deg)' },
                     '50%': { transform: 'rotate(3deg)' },
+                },
+                rotate360: {
+                    '100%': { transform: 'rotate(360deg)' },
+                },
+                movecircle: {
+                    '40%': { transform: 'translateY(20px) translateX(-30px)' }
                 }
             },
             animation: {
                 wiggle: 'wiggle 1s ease-in-out infinite',
+                rotate360: 'rotate360 40s infinite linear',
+                movecircle: 'movecircle 10s cubic-bezier(0.4, 0, 0.2, 1) infinite;'
             }
         },
     },
