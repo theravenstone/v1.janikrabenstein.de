7558763f5db47e3b4d1f74941327ec3e0850d2a2
theravenstone <theravenstonepsn@gmail.com>
2022-04-13 10:40:48 +0200

    fixed overflow issue

--- raven-frontend/src/components/Hero.vue
+++ raven-frontend/src/components/Hero.vue
@@ -9,19 +9,17 @@
           class="animate-wiggle mb-6 inline-block">👋</span></p>
     </div>
     <div class="hidden md:block">
-      <i id="triangle" class="fa-solid fa-play text-primary absolute w-24 h-24"></i>
-      <i id="square" class="fa-solid fa-square text-primary absolute animate-rotate360 -right-24 w-64 h-64"></i>
-      <i id="circle" class="fa-solid fa-circle text-primary absolute w-24 h-24 bottom-3/4 left-1/3 animate-movecircle"></i>
+      <i id="triangle" class="fa-brands fa-js-square text-primary absolute w-24 h-24"></i>
+      <i id="square" class="fa-brands fa-python text-primary absolute animate-rotate360 -right-24 w-64 h-64"></i>
+      <i id="circle" class="fa-brands fa-php text-primary absolute w-24 h-24 bottom-3/4 left-1/3 animate-movecircle"></i>
       <a rel="noopener" target="_blank" href="https://github.com/theravenstone" title="GitHub">
         <i id="github" class="fa-brands fa-github text-primary hover:text-primary-hover transition duration-300 absolute w-16 h-16 top-1/3"></i>
       </a>
-      <i id="vuejs" class="fa-brands fa-vuejs text-primary absolute w-16 h-16 left-3/4 top-3/4"></i>
+      <i id="vuejs" class="fa-brands fa-vuejs text-primary absolute w-16 h-16 left-2/3 top-3/4"></i>
       <a rel="noopener noreferrer" target="_blank" href="https://discord.com/users/594922360553865216" title="Discord">
         <i id="discord" class="fa-brands fa-discord text-primary hover:text-primary-hover transition duration-300 absolute w-16 h-16 left-3/4 top-1/4"></i>
       </a>
     </div>
-    
-    
     </section>
 </template>
 <script>
@@ -58,12 +56,12 @@ export default {
 @keyframes github-anim {
   50% {
     animation-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-    transform: translateY(15px) translateX(30px) rotate(-20deg);
+    transform: translateY(15px) translateX(20px) rotate(-20deg);
   }
 }
 #github {
   animation: github-anim 15s cubic-bezier(0.4, 0, 0.2, 1) infinite;
-  left: 13%;
+  left: 10%;
 }
 @keyframes vuejs-anim {
   50% {
