186250eede2177166365d8188204d788000c3796
theravenstone <theravenstonepsn@gmail.com>
2022-05-19 14:43:44 +0200

    typo

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    devServer: {
+        proxy: {
+            "^/api/": {
+                target: "https://janikrabenstein.de/api/",
+                pathRewrite: { "^/api/": "/api/" },
+                changeOrigin: true,
+                logLevel: "debug"
+            }
+        }
+    }
 })
