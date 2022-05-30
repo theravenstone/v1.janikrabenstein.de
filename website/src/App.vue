2c6cc8341a10abbf6272684935d66d69cc569d84
theravenstone <theravenstonepsn@gmail.com>
2022-05-30 09:42:41 +0200

    added animation on route change

--- website/src/App.vue
+++ website/src/App.vue
@@ -2,7 +2,11 @@
   <div class="bg-white text-slate-900 dark:bg-slate-900 dark:text-white transition duration-300">
     <Navbar />
 
-    <router-view />
+    <router-view v-slot="{Component}">
+    <transition name="fade" mode="out-in">
+      <component :is="Component" :key="$route.path"></component>
+    </transition>
+    </router-view>
 
     <Footer />  </div>
 </template>
@@ -21,3 +25,16 @@ export default {
 }
 </script>
 
+<style lang="css">
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+</style>
